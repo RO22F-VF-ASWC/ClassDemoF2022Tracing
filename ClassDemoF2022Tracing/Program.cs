5f5cbdb4208e3653a55dd8d140269a0dd8266e9d
Peter Levinsky <pele@easj.dk>
2022-02-18 12:49:37 +0100

    Including Configuration

--- ClassDemoF2022Tracing/Program.cs
+++ ClassDemoF2022Tracing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 
 namespace ClassDemoF2022Tracing
 {
@@ -6,10 +7,36 @@ namespace ClassDemoF2022Tracing
     {
         static void Main(string[] args)
         {
+            ReadConfiguration();
+
             AWorker worker = new AWorker();
             worker.Start();
 
             Console.WriteLine("Hello World!");
+        }
+
+        private static void ReadConfiguration()
+        {
+            XmlDocument configDoc = new XmlDocument();
+            configDoc.Load("Config.xml");
+
+            XmlNode portNode = configDoc.DocumentElement.SelectSingleNode("Port");
+            if (portNode != null)
+            {
+                String str = portNode.InnerText.Trim();
+                int serverPort = Convert.ToInt32(str);
+
+
+                Console.WriteLine("Read serverport is " + serverPort);
+            }
+
+            XmlNode nameNode = configDoc.DocumentElement.SelectSingleNode("Name");
+            if (nameNode != null)
+            {
+                String str = nameNode.InnerText.Trim();
+                
+                Console.WriteLine("Read server name is " + str);
+            }
         }
     }
 }
