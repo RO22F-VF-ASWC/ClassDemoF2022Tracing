016e4b1b89a4704771ae7bc057ccbc073b79a188
Peter Levinsky <pele@easj.dk>
2022-02-18 10:26:08 +0100

    Add project files.

---
+++ ClassDemoF2022Tracing/AWorker.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace ClassDemoF2022Tracing
+{
+    public class AWorker
+    {
+        // creating a trace source system  / object
+        private TraceSource ts = new TraceSource("Peters Demo");
+
+
+        public AWorker()
+        {
+            // setting the overall switch
+            ts.Switch = new SourceSwitch("Peters Log", "All");
+
+            // setting up listeners
+            TraceListener consoleLog = new ConsoleTraceListener();
+            ts.Listeners.Add(consoleLog);
+
+
+            TraceListener fileLog = new TextWriterTraceListener(new StreamWriter("TraceDemo.txt"));
+            fileLog.Filter = new EventTypeFilter(SourceLevels.Error);
+            ts.Listeners.Add(fileLog);
+
+
+            TraceListener xmlLog = new XmlWriterTraceListener(new StreamWriter("TraceDemo.xml"));
+            ts.Listeners.Add(xmlLog);
+
+            //TraceListener eventLog = new EventLogTraceListener("Application");
+            //ts.Listeners.Add(eventLog);
+
+        }
+
+        public void Start()
+        {
+            // do somthing 
+
+            // tracing
+            ts.TraceEvent(TraceEventType.Information, 333, "This is just information");
+            ts.TraceEvent(TraceEventType.Warning, 333, "This is a warning");
+            ts.TraceEvent(TraceEventType.Error, 333, "This is an error");
+            ts.TraceEvent(TraceEventType.Critical, 333, "!! Critical");
+
+            ts.Close();
+        }
+    }
+}
---
+++ ClassDemoF2022Tracing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassDemoF2022Tracing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AWorker worker = new AWorker();
+            worker.Start();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
